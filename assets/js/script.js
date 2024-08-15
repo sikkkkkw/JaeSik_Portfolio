3bc458b0f24d03627c0f38129ee280865a81c7f3
sikkkkkw <gksmsla92@naver.com>
2024-08-15 23:07:47 +0900

    로딩

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,4 +59,9 @@ window.addEventListener('click', function(event) {
         event.target.style.display = 'none';
     }
 });
-
+// 로딩
+window.addEventListener('load', function() {
+    // 페이지 로드 완료 시 로딩 화면을 숨김
+    const loadingScreen = document.getElementById('loading-screen');
+    loadingScreen.style.display = 'none';
+});
